04df587d769340d2bd1c9ebcc1f0e7bfc54645e3
jaron <jaron@commlinked.ph>
2018-08-13 16:53:42 +0800

    First Commit

---
+++ app/src/main/java/ph/commlinked/shoppinoy/splashActivity.kt
@@ -0,0 +1,12 @@
+package ph.commlinked.shoppinoy
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class splashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
